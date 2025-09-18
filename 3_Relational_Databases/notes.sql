6119616dc3582afdc519b269039eb74ed75578fc
yaslitasch <omer.yaslitas@ug.bilkent.edu.tr>
2025-09-18 03:43:44 +0300

    WIP: 3_Relational_Databases module

--- 3_Relational_Databases/notes.sql
+++ 3_Relational_Databases/notes.sql
@@ -234,3 +234,57 @@ SELECT * FROM professors;
 
 
 /* FOREIGN KEYS */
+
+-- 1:N
+-- Rename the university_shortname column
+ALTER TABLE professors
+RENAME COLUMN university_shortname TO university_id;
+
+-- Add a foreign key on professors referencing universities
+ALTER TABLE professors 
+ADD CONSTRAINT professors_fkey FOREIGN KEY (university_id) REFERENCES universities(id);
+
+-- JOIN tables linked by a foreign key
+
+-- Select all professors working for universities in the city of Zurich
+SELECT professors.lastname, universities.id, universities.university_city
+FROM professors
+JOIN universities
+ON professors.university_id = universities.id
+WHERE universities.university_city = 'Zurich';
+
+
+-- N:M relationships
+
+-- Add a professor_id column
+ALTER TABLE affiliations
+ADD COLUMN professor_id integer REFERENCES professors(id);
+
+-- Rename the organization column to organization_id
+ALTER TABLE affiliations
+RENAME organization TO organization_id;
+
+-- Add a foreign key on organization_id
+ALTER TABLE affiliations
+ADD CONSTRAINT affiliations_organization_fkey FOREIGN KEY (organization_id) REFERENCES organizations(id);
+
+SELECT * FROM affiliations;
+
+-- Set professor_id to professors.id where firstname, lastname correspond to rows in professors
+UPDATE affiliations
+SET professor_id = professors.id
+FROM professors
+WHERE affiliations.firstname = professors.firstname AND affiliations.lastname = professors.lastname;
+
+SELECT * FROM affiliations;
+
+-- Drop the firstname column
+ALTER TABLE affiliations
+DROP COLUMN firstname;
+
+-- Drop the lastname column
+ALTER TABLE affiliations
+DROP COLUMN lastname;
+
+SELECT * FROM affiliations;
+
